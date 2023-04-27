d9dd40e21e115330c296310b2539dc2fd71c5e55
bret99 <absolutedev@mail.ru>
2023-04-27 16:22:32 +0500

    RAM cleaner without notify

--- ram_cleaner_start.sh
+++ ram_cleaner_start.sh
@@ -1,10 +1,6 @@
 sudo sh -c 'echo "Enter the period for memory cleaning in seconds:"'
 read PERIOD
 
-notify() {
-    notify-send "Memory message" -t 5000 "$1"
-}
-
 main() {
     while true 
     do
@@ -12,7 +8,7 @@ main() {
         sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'
         RAM_AFTER=$(free -m|grep Mem|awk '{print $4}')
         RAM_RESQUE=$(($RAM_AFTER - $RAM_BEFORE))
-        notify "RAM resqued is $RAM_RESQUE Mbs"
+        echo -e "RAM resqued is \033[1;92m$RAM_RESQUE Mbs \033[1;00mat \033[1;90m$(date)\033[1;00m"
         sleep $PERIOD
     done
 }
